335fd03949adb4b4642da80ad3e1b7ddc1ebdb74
Saghar <saaghaar.kh@gmail.com>
2025-07-16 00:27:42 +0330

    separate score codes in the new file

---
+++ lib/game/score_manager.dart
@@ -0,0 +1,33 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class ScoreManager extends TextComponent {
+  int _score = 0;
+
+  ScoreManager(Vector2 screenSize)
+      : super(
+    text: 'Score: 0',
+    position: Vector2(screenSize.x / 2, 20),
+    anchor: Anchor.topCenter,
+    textRenderer: TextPaint(
+      style: const TextStyle(
+        fontSize: 24,
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    priority: 100,
+  );
+
+  void increaseScore(int value) {
+    _score += value;
+    text = 'Score: $_score';
+  }
+
+  int get score => _score;
+
+  void reset() {
+    _score = 0;
+    text = 'Score: 0';
+  }
+}
